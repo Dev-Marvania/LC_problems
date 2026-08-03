a5e8553f33a431cd07f500cf15ac15ae9011834a
Dev Marvania <dev.marvaniapvt@gmail.com>
2026-08-03 11:41:51 +0530

    Time: 3 ms (49.12%), Space: 21.2 MB (45.74%) - LeetHub

--- 0877-stone-game/0877-stone-game.py
+++ 0877-stone-game/0877-stone-game.py
@@ -1,12 +1,18 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
         n = len(piles)
+        dp = [[None] * n for _ in range(n)]
+
         if n%2 == 0:
             return True
 
         def maxdiff(i,j):
             if i==j:
                 return piles[i]
-            return max(piles[i]-maxdiff(i+1,j),piles[j]-maxdiff(i,j-1)) 
 
+            if dp[i][j] is not None:
+                return dp[i][j]
+
+            dp[i][j] = max(piles[i]-maxdiff(i+1,j),piles[j]-maxdiff(i,j-1)) 
+            return dp[i][j]
         return maxdiff(0,n-1) >0
